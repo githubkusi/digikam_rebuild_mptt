e3d11b2c10c3fb46350d5108edcf6bcbef72d75d
Markus Leuthold <git@titlis.org>
2017-11-06 00:04:43 +0100

    works

--- visualize_tree.py
+++ visualize_tree.py
@@ -1,5 +1,80 @@
+import MySQLdb as mdb
+import sys
+import networkx as nx
+
+
+class ModifiedPreorderTraversalTree:
+    def __init__(self, graph):
+        self.graph = graph
+        self.left = {}
+        self.right = {}
+
+    def next(self, node=0, counter=0):
+        counter += 1
+        self.left[node] = counter
+
+        for child_node in self.graph.neighbors(node):
+            counter = self.next(child_node, counter)
+
+        counter += 1
+        self.right[node] = counter
+        return counter
+
+    def calc_left_right(self, root_node=0):
+        self.next(root_node)
+        return self.left, self.right
+
+
+def get_connection_and_cursor():
+    con = mdb.connect(
+        host='localhost',
+        user='dkuser',
+        passwd='dkpasswd',
+        db='digikam_devel_core')
+
+    cursor = con.cursor()
+    return con, cursor
+
+
+def get_graph(cursor):
+    cursor.execute("select pid, id from Tags")
+    return cursor.fetchall()
+
+
+def write_left_right(cursor, left, right):
+    for node in left:
+        cursor.execute("update Tags set lft={}, rgt={} where id={}".format(left[node], right[node], node))
+
+
+def build_graph(data):
+    return nx.DiGraph(data)
+
+
+def build_graph_debug():
+    e = [(0, 1), (0, 2), (1, 3), (1, 4)]
+    graph = nx.DiGraph(e)
+    return graph
+
+
+def build_modified_preorder_traversal_tree(G):
+    mptt = ModifiedPreorderTraversalTree(G)
+    mptt.next(0,1)
+    return mptt.left, mptt.right
+
+
 def main():
-    print("asdf")
+    connection, cursor = get_connection_and_cursor()
+    data = get_graph(cursor)
+    graph = build_graph(data)
+    # graph = build_graph_debug()
+
+    mptt = ModifiedPreorderTraversalTree(graph)
+    left, right = mptt.calc_left_right(root_node=0)
+    print(left)
+    print(right)
+    write_left_right(cursor, left, right)
+    connection.commit()
+    connection.close()
 
 
 if __name__ == '__main__':
